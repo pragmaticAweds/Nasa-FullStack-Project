2ecb01c698b0a3d68465a39ecf0ab2a6134e1f25
AbdulAfeez Jimoh <jimohafeez738@gmail.com>
2023-02-01 08:45:42 +0100

    setting up launch

--- server/src/components/launches/launches.controller.js
+++ server/src/components/launches/launches.controller.js
@@ -1,7 +1,7 @@
-const launchesModel = require("./launches.model");
+const { launches } = require("./launches.model");
 
 function getAllLaunches(req, res) {
-  return "hello";
+  return res.status(200).json(Array.from(launches.values()));
 }
 
 module.exports = {
