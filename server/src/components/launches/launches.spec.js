7b7f0335b107db5606d22b4a5bd4579d03310212
AbdulAfeez Jimoh <jimohafeez738@gmail.com>
2023-02-05 10:30:19 +0100

    testing, handling post

--- server/src/components/launches/launches.spec.js
+++ server/src/components/launches/launches.spec.js
@@ -2,14 +2,42 @@ const request = require("supertest");
 const app = require("../../app");
 
 describe("Test Get Launches", () => {
-  test("It should respond with 200", () => {
-    const response = request(app).get("/launches");
-    expect(response).toBe(200);
+  test("It should respond with 200", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-type", /json/)
+      .expect(200);
   });
 });
 
 describe("Test Post Launches", () => {
-  test("It should resppond with 201 success", () => {});
+  const newLaunch = {
+    mission: "Launch EnterPrise",
+    rocket: "NCC-101",
+    target: "kepler-186 f",
+    launchDate: "January 4, 2028",
+  };
+
+  const newLaunchWithNoDate = {
+    mission: "Launch EnterPrise",
+    rocket: "NCC-101",
+    target: "kepler-186 f",
+  };
+
+  test("It should respond with 201 success", async () => {
+    const res = await request(app)
+      .post("/launches")
+      .expect("Content-type", /json/)
+      .send(newLaunch)
+      .expect(201);
+
+    const reqDate = new Date(newLaunch.launchDate).valueOf();
+    const resDate = new Date(res.body.launchDate).valueOf();
+
+    expect(reqDate).toBe(resDate);
+    expect(res.body).toMatchObject(newLaunchWithNoDate);
+  });
+
   test("It should catch missing required property", () => {});
   test("It should catch invalid dates", () => {});
 });
